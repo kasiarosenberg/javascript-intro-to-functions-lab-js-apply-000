9806e0c09e38b77983892a1949e71d03bcd71d88
kate ROSENBERG <kate@digitalmachinations.com>
2018-02-22 20:31:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function describe(string) {
 }
 
 function sayHiToGrandma(string) {
-  if(string.toLowerCase === string {
+  if(string.toLowerCase === string) {
     return "I can\'t hear you!";
   } else if(string.toUpperCase() === string) {
     return "YES INDEED!";
